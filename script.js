3956841e77e27cf2b86843c455e95da39be2fac7
Sviatoslav <svitoslav@example.com>
2020-03-07 10:41:02 +0200

    add single round

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function getCells() {
     return document.getElementsByClassName("cell");
 }
 
-
 function cellClick(evt) {
     const cell = evt.target;
     const color = cell.getAttribute("data-color");
@@ -15,6 +14,48 @@ function getRandomColor() {
     return colors[randomIndex];
 }
 
+function startNewRound() {
+    colorCells();
+    const targetColor = getRandomColor();
+    assignCellClickHandlers(targetColor);
+    displayStatusBarMessage("Memorize...");
+    window.setTimeout(
+        () => { displayRoundTask(targetColor); },
+        5000
+    )
+}
+
+function countCellsByColor(targetColor) {
+    var count = 0;
+    const cells = getCells();
+    for(var i = 0; i < cells.length; i++) {
+        const cell = cells[i];
+        const cellColor = cell.getAttribute("data-color");
+        if(cellColor === targetColor)
+            count++;
+    }
+    return count;
+}
+
+function assignCellClickHandlers(targetColor) {
+    var counter = countCellsByColor(targetColor);
+    const cells = getCells();
+    for(var i = 0; i < cells.length; i++) {
+        const cell = cells[i];
+        cell.addEventListener("click", () => {
+            const cellColor = cell.getAttribute("data-color");
+            if(cellColor !== targetColor) {
+                loseRound();
+                counter = -1;
+            } else {
+                counter--;
+                if(counter === 0)
+                winRound();
+            }
+        });
+    }
+}
+
 function colorCells() {
     const cells = getCells();
     for (var i = 0; i < cells.length; i++) {
@@ -24,11 +65,23 @@ function colorCells() {
         cell.setAttribute("data-color", color);
         window.setTimeout(
             () => { cell.style.backgroundColor = null; },
-            5000
+            3000
         )
     }
 }
 
-function initialize() {
-    colorCells();
+function displayRoundTask(targetColor) { 
+    displayStatusBarMessage("Click all cells of " + targetColor + " color");
+}
+
+function loseRound() {
+    displayStatusBarMessage("Lost :-(");
+}
+function winRound() { 
+    displayStatusBarMessage("Win :-)");
+}
+
+function displayStatusBarMessage(message) { 
+    const statusBar = document.getElementById('status-bar');
+    statusBar.innerText = message;
 }
